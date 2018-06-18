dabc7b0d653a4bce6cdb3cde998aec520682c50f
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-18 19:59:10 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/fragmentcommunicator1903/FragmentiOS.java
@@ -0,0 +1,19 @@
+package com.ptp.phamtanphat.fragmentcommunicator1903;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentiOS extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_ios,container,false);
+        return view;
+    }
+}
